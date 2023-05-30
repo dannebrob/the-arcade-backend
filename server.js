0c4701b5ce1c056974fb7d2ec4d8d370bf47c76f
tess.aquilon <tess.aquilon@gmail.com>
2023-05-30 13:58:12 +0200

    added user schema and authentication

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -13,8 +20,93 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Fuckface!");
 });
+
+
+/////////////// Authentication
+// Create user 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Create registration 
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  // Hash the password
+  try {
+    const salt = bcrypt.genSaltSync();
+    const newUser = await new User({
+      username: username,
+      password: bcrypt.hashSync(password, salt)
+    }).save();
+    if (newUser) {
+      res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          id: newUser._id,
+          accessToken: newUser.accessToken
+        }
+      })
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Could not register user"
+      })
+
+    } 
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      response: e
+    })
+  }
+});
+
+// Create login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    // const user = await User.findOne({ username: username })
+    const user = await User.findOne({ username })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        }
+      })
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Credentials do not match"
+      })
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e
+    })
+  }
+})
+/////////////// Authentication end
 
 // Start the server
 app.listen(port, () => {
