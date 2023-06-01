fb9172b6f0e4d888e86ef3d7ab4a32c52ba17e01
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-01 19:37:48 +0200

    Fetching to db function working

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import crypto from "crypto";
-import bcrypt from "bcrypt";
+import express from 'express';
+import cors from 'cors';
+import mongoose, { Schema } from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-mongo"
+const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1/the-arcade2';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -19,8 +19,34 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Fuckface!");
+app.get('/', (req, res) => {
+  res.send('Hello Fuckface!');
+});
+
+//database schema
+const gameSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true
+  },
+  cover: {
+    id: {
+      type: Number,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  }
 });
 
 // Create user
@@ -36,7 +62,7 @@ const userSchema = new mongoose.Schema({
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString("hex")
+    default: () => crypto.randomBytes(128).toString('hex')
   },
   createdAt: {
     type: Date,
@@ -49,7 +75,7 @@ const userSchema = new mongoose.Schema({
 
 // Create review
 
-const  reviewSchema = new mongoose.Schema({
+const reviewSchema = new mongoose.Schema({
   message: {
     type: String
   },
@@ -57,7 +83,7 @@ const  reviewSchema = new mongoose.Schema({
     type: Date,
     default: () => new Date()
   }
-  
+
   // Some type of like functionality?
 
   // user: {
@@ -66,7 +92,62 @@ const  reviewSchema = new mongoose.Schema({
   // }
 });
 
-const User = mongoose.model("User", userSchema);
+const Game = mongoose.model('Game', gameSchema);
+const User = mongoose.model('User', userSchema);
+
+// populate database with games
+const fetchAndSaveGames = async (offset, batchSize) => {
+  try {
+    const response = await fetch('https://api.igdb.com/v4/games', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'bearer x3szpni9f7z2x85qr6wycf78bvddyt',
+        'Client-ID': 'b5um59369k9qvopt63t0x23klw3umq',
+        Accept: 'application/json'
+      },
+      body: `fields name,summary,cover.url; limit ${batchSize}; offset ${offset};`
+    });
+    const games = await response.json();
+    console.log(games);
+
+    // Save the games to your database
+    const newUser = await new Game(...games).save();
+    // Insert the games into the appropriate collection/table
+
+    return games.length; // Return the number of games fetched in this batch
+  } catch (error) {
+    console.error(error);
+    return 0;
+  }
+};
+
+const fetchAllGames = async () => {
+  const batchSize = 1; // Number of games to fetch in each batch
+  const totalGames = 10000; // Total number of games to fetch
+
+  const delay = 250; // Delay in milliseconds (4 requests per second)
+  let offset = 0; // Initial offset
+
+  try {
+    let totalCount = 0;
+    let fetchedCount = 0;
+
+    while (totalCount < totalGames) {
+      fetchedCount = await fetchAndSaveGames(offset, batchSize);
+      totalCount += fetchedCount;
+      offset += batchSize;
+
+      // Delay between API calls
+      if (totalCount < totalGames) {
+        await new Promise((resolve) => setTimeout(resolve, delay));
+      }
+    }
+
+    console.log(`Total games fetched and saved: ${totalCount}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 // A Review model is not necessarily needed, but can be handy for additional review
 // specific functionalities, such as adding likes, comments
@@ -76,7 +157,7 @@ const User = mongoose.model("User", userSchema);
 
 // Register user
 
-app.post("users/register", async (req, res) => {
+app.post('users/register', async (req, res) => {
   const { username, password } = req.body;
   // Hash the password
   try {
@@ -94,50 +175,52 @@ app.post("users/register", async (req, res) => {
           accessToken: newUser.accessToken,
           createdAt: newUser.createdAt
         }
-      })
+      });
     } else {
       res.status(400).json({
         success: false,
-        response: "Could not register user"
-      })
-
-    } 
+        response: 'Could not register user'
+      });
+    }
   } catch (e) {
     res.status(400).json({
       success: false,
-      message: "Could not register user",
+      message: 'Could not register user',
       response: e
-    })
+    });
   }
 });
 
 // Authenticate user
 const authenticateUser = async (req, res, next) => {
-  const accessToken = req.header("Authorization");
+  const accessToken = req.header('Authorization');
   try {
-    const user = await User.findOne({accessToken})
+    const user = await User.findOne({ accessToken });
     if (user) {
       next();
     } else {
       res.status(401).json({
-        success:false,
-        response: "Please log in"
-      })
+        success: false,
+        response: 'Please log in'
+      });
     }
   } catch (e) {
     res.status(500).json({
       success: false,
-      message: "Could not authenticate user",
+      message: 'Could not authenticate user',
       response: e
-    })
+    });
   }
 };
 
+// get game data
+app.get('/fetch-games', fetchAllGames);
+
 // Login user
-app.post("users/login", async (req, res) => {
+app.post('users/login', async (req, res) => {
   const { username, password } = req.body;
   try {
-    const user = await User.findOne({ username })
+    const user = await User.findOne({ username });
     if (user && bcrypt.compareSync(password, user.password)) {
       res.status(201).json({
         success: true,
@@ -147,101 +230,105 @@ app.post("users/login", async (req, res) => {
           accessToken: user.accessToken,
           createdAt: user.createdAt
         }
-      })
+      });
     } else {
       res.status(400).json({
         success: false,
-        message: "Credentials do not match"
-      })
+        message: 'Credentials do not match'
+      });
     }
   } catch (e) {
     res.status(500).json({
       success: false,
-      message: "Could not log in user",
+      message: 'Could not log in user',
       response: e
-    })
+    });
   }
 });
 
 // Retrieve user details by ID
 
-app.get("/users/:_id", async (req, res) => {
+app.get('/users/:_id', async (req, res) => {
   const userId = req.params._id;
   const user = await User.findById(userId);
   try {
     if (user) {
       res.status(200).json({
         success: true,
-        response: user})
+        response: user
+      });
     } else {
       res.status(400).json({
-        success: false, 
-        message: "Faulty user ID",
-        response: e})
+        success: false,
+        message: 'Faulty user ID',
+        response: e
+      });
     }
-  } catch(e) {
+  } catch (e) {
     res.status(500).json({
       success: false,
-      message: "Could not GET user details",
+      message: 'Could not GET user details',
       response: e
-    })
-  } 
+    });
+  }
 });
-
 
 // Update user details
 
-app.patch("/users/:_id", authenticateUser, async (req, res) => {
+app.patch('/users/:_id', authenticateUser, async (req, res) => {
   const userId = req.params._id;
   const updates = req.body;
   try {
-    const updatedUser = await User.findByIdAndUpdate(userId, updates, {new: true});
+    const updatedUser = await User.findByIdAndUpdate(userId, updates, {
+      new: true
+    });
     if (updatedUser) {
       res.status(200).json({
         success: true,
-        response: updatedUser});
+        response: updatedUser
+      });
     } else {
       res.status(400).json({
         success: false,
-        message: "Faulty user ID",
+        message: 'Faulty user ID',
         response: e
-      })
+      });
     }
-  } catch(e) {
-      res.status(500).json({
-        success: false,
-        message: "Could not update user details",
-        response: e
-      })
-    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: 'Could not update user details',
+      response: e
+    });
   }
-);
+});
 
 // Delete user
 
-app.delete("/users/:_id", authenticateUser, async (req, res) => {
+app.delete('/users/:_id', authenticateUser, async (req, res) => {
   const userId = req.params._id;
   try {
     const deletedUser = await User.findByIdAndDelete(userId);
     if (deletedUser) {
       res.status(200).json({
         success: true,
-        response: deletedUser});
+        response: deletedUser
+      });
     } else {
       res.status(400).json({
         success: false,
-        message: "Faulty user ID",
+        message: 'Faulty user ID',
         response: e
-      })
+      });
     }
-  } catch(e) {
-      res.status(500).json({
-        success: false,
-        message: "Could not delete user",
-        response: e
-      })
-    }
-  });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: 'Could not delete user',
+      response: e
+    });
+  }
+});
 
 /////////////////////// User Endpoints END
 
@@ -249,111 +336,119 @@ app.delete("/users/:_id", authenticateUser, async (req, res) => {
 
 // Endpoint to post new review (when logged in)
 
-app.post("/reviews", authenticateUser);
-app.post("/reviews", async (req, res) => {
+app.post('/reviews', authenticateUser);
+app.post('/reviews', async (req, res) => {
   const { message } = req.body;
-  const accessToken = req.header("Authorization");
-  const user = await User.findOne({accessToken})
-  const reviews = await new Review({message: message, user: user._id}).save()
-  // try catch, if else
-  try {
-    if (reviews) {
-      res.status(200).json({
-        success: true, 
-        response: reviews})
-    } else {
-      res.status(400).json({
-        success: false, 
-        response: "Please log in to POST review"})
-    }
-  } catch(e) {
-    res.status(500).json({
-      success: false,
-      response: e
-    })
-  }
-});
-
-// Endpoint to delete one specific review by one specific user (when logged in)
-
-app.delete("/reviews/:_id", authenticateUser);
-app.delete("/reviews/:_id", async (req, res) => {
-  const accessToken = req.header("Authorization");
-  const user = await User.findOne({accessToken});
-  const deletedReview = await Review.findByIdAndDelete(req.params._id);
-  try {
-    if (deletedReview) {
-      res.status(200).json({
-        success: true, 
-        message: "Review successfully deleted",
-        deletedReview: deletedReview})
-    } else {
-      res.status(400).json({
-        success: false, 
-        message: "Please log in to DELETE review",
-        response: e})
-    }
-  } catch(e) {
-    res.status(500).json({
-      success: false,
-      message: "Faulty ID",
-      response: e
-    })
-  }
-});
-
-// Endpoint to find all reviews by one specific user (when logged in)
-
-app.get("/reviews", authenticateUser);
-app.get("/reviews", async (req, res) => {
-  const accessToken = req.header("Authorization");
-  const user = await User.findOne({accessToken})
-  const reviews = await Review.find({user: user._id});
+  const accessToken = req.header('Authorization');
+  const user = await User.findOne({ accessToken });
+  const reviews = await new Review({ message: message, user: user._id }).save();
   // try catch, if else
   try {
     if (reviews) {
       res.status(200).json({
         success: true,
-        response: reviews})
+        response: reviews
+      });
     } else {
       res.status(400).json({
-        success: false, 
-        message: "Please log in to GET reviews",
-        response: e})
+        success: false,
+        response: 'Please log in to POST review'
+      });
     }
-  } catch(e) {
+  } catch (e) {
     res.status(500).json({
       success: false,
       response: e
-    })
-  } 
+    });
+  }
+});
+
+// Endpoint to delete one specific review by one specific user (when logged in)
+
+app.delete('/reviews/:_id', authenticateUser);
+app.delete('/reviews/:_id', async (req, res) => {
+  const accessToken = req.header('Authorization');
+  const user = await User.findOne({ accessToken });
+  const deletedReview = await Review.findByIdAndDelete(req.params._id);
+  try {
+    if (deletedReview) {
+      res.status(200).json({
+        success: true,
+        message: 'Review successfully deleted',
+        deletedReview: deletedReview
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: 'Please log in to DELETE review',
+        response: e
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: 'Faulty ID',
+      response: e
+    });
+  }
+});
+
+// Endpoint to find all reviews by one specific user (when logged in)
+
+app.get('/reviews', authenticateUser);
+app.get('/reviews', async (req, res) => {
+  const accessToken = req.header('Authorization');
+  const user = await User.findOne({ accessToken });
+  const reviews = await Review.find({ user: user._id });
+  // try catch, if else
+  try {
+    if (reviews) {
+      res.status(200).json({
+        success: true,
+        response: reviews
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: 'Please log in to GET reviews',
+        response: e
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e
+    });
+  }
 });
 
 // Endpoint to find one specific review by one specific user (when logged in)
 
-app.get("/reviews/:_id", authenticateUser);
-app.get("/reviews/:_id", async (req, res) => {
-  const accessToken = req.header("Authorization");
-  const user = await User.findOne({accessToken});
+app.get('/reviews/:_id', authenticateUser);
+app.get('/reviews/:_id', async (req, res) => {
+  const accessToken = req.header('Authorization');
+  const user = await User.findOne({ accessToken });
   const singleReview = await Review.findById(req.params._id);
   // try catch, if else
   try {
     if (singleReview) {
       res.status(200).json({
-        success: true, 
-        response: singleReview})
+        success: true,
+        response: singleReview
+      });
     } else {
       res.status(400).json({
-        success: false, 
-        message: "Please log in to GET review",
-        response: e})
+        success: false,
+        message: 'Please log in to GET review',
+        response: e
+      });
     }
-  } catch(e) {
+  } catch (e) {
     res.status(500).json({
       success: false,
-      message: "Faulty ID",
+      message: 'Faulty ID',
       response: e
-    })
+    });
   }
 });
 
@@ -363,49 +458,53 @@ app.get("/reviews/:_id", async (req, res) => {
 
 // Endpoint to get all games
 
-app.get("/games", async (req, res) => {
+app.get('/games', async (req, res) => {
   const games = await Game.find();
   try {
     if (games) {
       res.status(200).json({
         success: true,
-        response: games})
+        response: games
+      });
     } else {
       res.status(400).json({
-        success: false, 
-        message: "Could not GET games",
-        response: e})
+        success: false,
+        message: 'Could not GET games',
+        response: e
+      });
     }
-  } catch(e) {
+  } catch (e) {
     res.status(500).json({
       success: false,
       response: e
-    })
-  } 
+    });
+  }
 });
 
 // Endpoint to get one specific game
 
-app.get("/games/:_id", async (req, res) => {
+app.get('/games/:_id', async (req, res) => {
   const singleGame = await Game.findById(req.params._id);
   try {
     if (singleGame) {
       res.status(200).json({
         success: true,
-        response: singleGame})
+        response: singleGame
+      });
     } else {
       res.status(400).json({
-        success: false, 
-        message: "Could not GET game",
-        response: e})
+        success: false,
+        message: 'Could not GET game',
+        response: e
+      });
     }
-  } catch(e) {
+  } catch (e) {
     res.status(500).json({
       success: false,
-      message: "Faulty ID",
+      message: 'Faulty ID',
       response: e
-    })
-  } 
+    });
+  }
 });
 
 /////// Filtering and sorting
@@ -413,28 +512,29 @@ app.get("/games/:_id", async (req, res) => {
 // Retrieve games based on specific category
 // Example: /games?category=retro
 
-app.get("/games", async (req, res) => {
+app.get('/games', async (req, res) => {
   const category = req.query.category;
-  const games = await Game.find({category: category});
+  const games = await Game.find({ category: category });
   try {
     if (games) {
       res.status(200).json({
         success: true,
-        response: games})
+        response: games
+      });
     } else {
       res.status(400).json({
-        success: false, 
-        message: "Could not GET games",
-        response: e})
+        success: false,
+        message: 'Could not GET games',
+        response: e
+      });
     }
-  } catch(e) {
+  } catch (e) {
     res.status(500).json({
       success: false,
       response: e
-    })
-  } 
+    });
+  }
 });
-
 
 // Start the server
 app.listen(port, () => {
