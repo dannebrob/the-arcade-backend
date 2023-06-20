615d720160cf072273bdf53821790c2839f60d2d
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-20 21:31:05 +0200

    Revert "fix of auth, missing games model"
    This reverts commit bfaf058e4156eb26fcd0f7b2afbab7e55405b18d.

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import User from '../models/user.js';
-import Game from '../models/game.js';
 import Review from '../models/review.js';
 import authenticateUser from '../middleware/authenticateUser.js';
 
