57c0189bd51936b500b033366bc930fa43ad8e2e
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-06-21 14:57:56 +0200

    new fix of server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import mongoose, { Schema } from 'mongoose';
 import expressListEndpoints from 'express-list-endpoints';
 import 'dotenv/config';
 
-import createRouter from './routes/create.js';
 import fetchRouter from './routes/fetchGames.js';
 import gameRouter from './routes/games.js';
 import reviewRouter from './routes/reviews.js';
@@ -30,11 +29,32 @@ app.use(cors());
 app.use(express.json());
 
 // Adds the Route's file's routes to the application at the root path
-app.use('/', createRouter);
 app.use('/', fetchRouter);
 app.use('/', gameRouter);
 app.use('/', reviewRouter);
 app.use('/', userRouter);
+
+const { Configuration, OpenAIApi } = require('openai');
+const { OPENAI_API_KEY } = process.env;
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.post('/create', async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '512x512'
+    });
+    res.send(response.data.data[0].url);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
 
 // Start defining your routes here
 app.get('/', (req, res) => {
